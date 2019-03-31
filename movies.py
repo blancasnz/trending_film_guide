3a4b199e89b72daafc22578634101568f59396ab
Blanca Sanchez <blancayllen@gmail.com>
2019-03-31 14:21:59 -0700

    added styling

--- movies.py
+++ movies.py
@@ -12,15 +12,14 @@ movies = [
     }
 ]
 
-
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', movies=movies)
 
 @app.route("/movie")
 def movie():
-    return render_template('movie.html')
+    return render_template('movie.html', title='Particular movie')
 
 if __name__ == '__main__':
     app.run(debug=True)
