f2e38c362c07d0ffeebbd4faa8c728eb142e1eac
Blanca Sanchez <blancayllen@gmail.com>
2019-03-30 22:42:55 -0700

    created templates

---
+++ movies.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+movies = [
+    {
+        'title' : 'Whiplash',
+        'director' : 'Damien Chazelle',
+    },
+    {
+        'title' : 'Lord of the Rings',
+        'director' : 'Peter Jackson'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', movies=movies)
+
+@app.route("/movie")
+def movie():
+    return render_template('movie.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
